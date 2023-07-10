b2bc6f915c0875d7d9c88cf08972b98ca9be6f0c
C'est La Vie <18225083149@163.com>
2023-07-10 17:54:12 +0800

    1

---
+++ components/cardinfo/cardinfo.vue
@@ -0,0 +1,290 @@
+<template>
+	<view>
+		<!-- 任务卡片 -->
+		<view class="task{{task.$id%3}} columnlayout" >
+		  <!-- 上部分，包括： 标题，类型，工时，回馈 两行 -->
+		  <view class="task-top rowlayout">
+		    <!-- 左半部分，包括：标题，工时，回馈比 -->
+		    <view class="task-top-left columnlayout">
+		    <!-- 第一层：需求内容 编号 -->
+		      <view class="rowlayout">
+		        <view class="fontcolor{{task.$id%3}} poster" >需求内容</view>
+		        <!-- 序列号 -->
+		        <view class="serialNo">k8963245{{task.id}}</view>
+		      </view>
+		      <!-- 第二层：标题 -->
+		      <view class="title">
+		        <textarea disabled="{{!editable}}" class="brief" value="{{task.title}}" placeholder="一句话简述任务内容">
+		        </textarea>
+		      </view>
+		      <!-- 第三层：预计工时，回馈 -->
+		      <view class="timeandreward">
+		          <!-- 预计工时 -->
+		          <view class="columnlayout spendtime">
+		            <view class="fontcolor{{task.$id%3}}">预计工时</view>
+		            <view class="rowlayout">
+		              <input maxlength="8" disabled="{{!editable}}" type="digit" 
+		              value="{{ editable? '':spendtime }}" class="input" />h
+		            </view>
+		            
+		          </view>
+		          <!-- 回馈值 -->
+		          <view class="columnlayout reward">
+		            <view class="fontcolor{{task.$id%3}}">回馈值</view>
+		            <view class="rowlayout">
+		              <view style="width: 40px;border-bottom: 1px dashed gray;">{{task.reward}}</view>
+		              <view>{{ rewardtype.value }}</view>
+		              <t-dropdown-menu style="width: 20px;height: 20px;">
+		                <t-dropdown-item style="z-index: 99;align-self: center;align-items: center;align-content: center;text-align: right;" 
+		                options="{{ rewardtype.options}}" value="{{rewardtype.label}}"
+		                 bindchange="onChange" />
+		              </t-dropdown-menu>
+		            </view>
+		          </view>
+		          
+		        </view>
+		    </view>
+		    <!-- 右半部分，只包括：类型 -->
+		    <view class="task-top-right columnlayout" >
+		        <view class="tasktype columnlayout">{{ tasktype }}</view>
+		    </view>
+		  </view>
+		  <!-- 部门，发起人，状态 -->
+		  <view class="task-bottom rowlayout">
+		   <!-- 部门 -->
+		   <!-- range-key 用于指定显示名称属性值 -->
+		    <picker disabled="{{!editable}}" range="{{departments}}" range-key="{{'name'}}"
+		     class="department fontcolor{{task.$id%3}}" bindchange="bindPickerChange">
+		     {{editable ? departments[index]["name"] : departments[task.branchid]["name"]}}</picker>
+		    <!-- 发起人 -->
+		    <view class="organigerpart" wx:if="{{ !editable }}">
+		      <view>{{task.username}}</view>
+		      <view class="fontcolor{{task.$id%3}}">发起人</view>
+		    </view>
+		    <!-- 状态 -->
+		    <view class="status" hidden="{{editable}}">{{status[task.status]}}</view>
+		  </view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		name:"cardinfo",
+		props:{
+		    task:{},
+		    //颜色代码
+		    colorid:"1",
+		    //可编辑组件（input,textarea等）是否能编辑，默认不能编辑
+		    editable:false
+		},
+		data() {
+			return {
+				    // 预计时间
+				    spendtime:"",
+				    _deparment:"",
+				    index:0,
+				    departments:[],
+				    tasktype:"类型",
+				    status:["代接","完成","审核中"],
+				    rewardtype: {
+				      value: '￥',
+				      options: [
+				        {
+				          label: '元',
+				          value: '￥',
+				        },
+				        {
+				          lable: '百分比',
+				          value: '%',
+				        },]
+				      }
+			};
+		}
+	}
+</script>
+
+<style lang="less">
+	@import url('../../common/common.less');
+	.size(@width,@height){
+	  width: @width;
+	  height: @height;
+	}
+	
+	.example1{
+	  .size(200px,300px);
+	}
+	
+	.colorset(@color){
+	  color: @color;
+	}
+	//
+	.fontcolor0{
+	  .colorset(rgb(30, 30, 245));
+	}
+	.fontcolor1{
+	  color: rgb(20, 226, 226);
+	}
+	.fontcolor2{
+	  color: rgb(45, 145, 238);
+	}
+	
+	.task(){
+	  height: 195px;
+	  width: 100%;
+	  align-self: center;
+	  position: relative;
+	  border: 2px solid #04882c;
+	  margin: 0px 5px;
+	  z-index: 0;
+	}
+	.taskbefore(){
+	  top: 0;
+	  left: 0;
+	  right: 0;
+	  bottom: 0;
+	  content: "";
+	  position: absolute;
+	  clip-path: circle(11rem at 100% 2rem);
+	  z-index: -1;
+	}
+	
+	.task0{
+	  .task();
+	}
+	
+	.task0:before{
+	  .taskbefore();
+	  background-color: rgb(30, 30, 245);
+	
+	}
+	
+	.task1{
+	  .task();
+	}
+	
+	.task1:before{
+	  .taskbefore();
+	  background-color: rgb(20, 226, 226);
+	}
+	
+	.task2{
+	  .task();
+	}
+	
+	.task2:before{
+	  .taskbefore();
+	  background-color: rgb(45, 145, 238);
+	}
+	
+	
+	.task-top{
+	height: 70%;
+	z-index: 99;
+	}
+	
+	.task-bottom{
+	display: flex;
+	flex-direction: row;
+	z-index: 99;
+	}
+	
+	.task-top-left{
+	width: 50%;
+	}
+	
+	.task-top-right{
+	  width: 50%;
+	  align-self: flex-end;
+	  align-content: flex-end;
+	}
+	
+	.tasktype{
+	  bottom: 0px;
+	  right: 0px;
+	  font-size: 2cm;
+	  font-style: italic;
+	  color:white;
+	  //position:fixed;
+	  // align-self: flex-end;
+	  // align-content: flex-end;
+	}
+	
+	.margin-top5{
+	  margin-top: 5px;
+	}
+	
+	//预计用时
+	.spendtime{
+	  width: 50%;
+	  margin-right: 5px;
+	}
+	
+	//回馈
+	.reward{
+	  width: 50%;
+	  //margin-top: 5px;
+	}
+	
+	.margin-right10{
+	  margin-right:10px;
+	}
+	.serialNo{
+	  display: flex;
+	  flex-direction: column;
+	  align-self: flex-end;
+	  font-size: x-small;
+	  margin-left: 2px;
+	}
+	
+	.poster{
+	  //.italic();
+	  margin-right: 2px;
+	}
+	
+	.department{
+	  .margin-right10();
+	  font-size: 1.0cm;
+	}
+	
+	.organigerpart{
+	  .columnlayout();
+	  .margin-right10();
+	  display: flex;
+	  padding-bottom: 5px;
+	  align-self: flex-end;
+	}
+	
+	.status{
+	  //.margin-right10();
+	  margin-left: 20px;
+	  height: 32px;
+	  border: 2px solid rgb(241, 91, 4);
+	  border-radius: 50%;
+	  color: white;
+	  display: flex;
+	  align-items: center;
+	  text-align: center;
+	}
+	.timeandreward{
+	  .rowlayout();
+	  .margin-top5();
+	}
+	
+	.title{
+	  .blackcolor();
+	  .margin-top5();
+	  height: 62%;
+	}
+	.input{
+	  width: 60%;
+	  text-align: center;
+	  border-bottom: 1px dashed gray;
+	  //border-bottom: 1px,rgb(10, 221, 63),dashed;
+	}
+	.brief{
+	  .title();
+	  width: 100%;
+	  white-space: pre-wrap;
+	}
+</style>
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,9 +1,13 @@
 <template>
 	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view class="text-area">
-			<text class="title">{{title}}</text>
-		</view>
+		<uni-search-bar class="uni-mt-10" radius="5" placeholder="搜索任务" clearButton="auto" cancelButton="none" @confirm="search" />
+		<swiper class="swiper" circular :indicator-dots="true" :autoplay="true" :interval="2000"
+				:duration="500">
+			<swiper-item><view class="swiper-item uni-bg-red">A</view></swiper-item>
+			<swiper-item><view class="swiper-item uni-bg-green">B</view></swiper-item>
+			<swiper-item><view class="swiper-item uni-bg-blue">C</view></swiper-item>
+		</swiper>
+		<cardinfo :task="{}" editable="true"></cardinfo>
 	</view>
 </template>
 
@@ -11,14 +15,47 @@
 	export default {
 		data() {
 			return {
-				title: 'Hello'
+				title: 'Hello',
+				tasks:[]
 			}
 		},
 		onLoad() {
-
+			    try {
+			      const data = await fetchTasks("/Assignment/");
+			      console.log(data.$values);  // 查看返回的数据
+			      this.setData({
+			        //使用data.$values,我使用后端框架的默认数据格式，后面会调整
+			        tasks: data.$values
+			      });
+			    } catch (error) {
+			      console.error("Error getting data from the API:", error);
+			    }
 		},
 		methods: {
-		
+			async function fetchTasks(urlpath) {
+			  return new Promise((resolve, reject) => {
+				uni.request({
+				  //直接使用地址拼接
+				  url: `${app.globalData.apiBaseUrl}`+urlpath,
+				  method: "GET",
+				  // data 模式会转换成url参数，也就是url会转换成 ${app.globalData.apiBaseUrl}/Assignment?userId=
+				  //但真实的请求应该是 ${app.globalData.apiBaseUrl}/Assignment/userId
+				  // data: {
+				  //   userId: userId,
+				  // },
+				  success: (res) => {
+					if (res.statusCode === 200) {
+					  resolve(res.data);
+					} else {
+					  reject(res);
+					}
+				  },
+				  fail: (err) => {
+					reject(err);
+				  },
+				});
+			  });
+			},
 		}
 	}
 </script>
@@ -27,8 +64,8 @@
 	.content {
 		display: flex;
 		flex-direction: column;
-		align-items: center;
-		justify-content: center;
+		//align-items: center;
+		//justify-content: center;
 	}
 
 	.logo {
@@ -48,5 +85,29 @@
 	.title {
 		font-size: 36rpx;
 		color: #8f8f94;
+	}
+	
+	.uni-mt-10 {
+		margin-top: 10px;
+		display: block;
+	}
+	
+	.swiper {
+			height: 300rpx;
+		}
+	.swiper-item {
+		display: block;
+		height: 300rpx;
+		line-height: 300rpx;
+		text-align: center;
+	}
+	.uni-bg-red{
+		background-color: red;
+	}
+	.uni-bg-green{
+		background-color: green;
+	}
+	.uni-bg-blue{
+		background-color: blue;
 	}
 </style>
