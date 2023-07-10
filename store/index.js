38e62c78a72aa597a731ca098924f890b60a0b1c
C'est La Vie <18225083149@163.com>
2023-07-11 00:18:51 +0800

    1

--- store/index.js
+++ store/index.js
@@ -1,5 +1,28 @@
 import {createStore} from "vuex"
 
 const store = createStore({
-	
+	state:{
+		hasLogin:false,
+		branchs:[],
+		departments:[]
+	},
+	mutations:{
+		getBranchs(state){
+			
+		},
+		getDepartments(state){
+			
+		}
+	},
+	getters:{
+		
+	},
+	actions:{
+		getTasks({options}){
+			
+		},
+		getTaskById({commit},id){
+			
+		}
+	}
 })
